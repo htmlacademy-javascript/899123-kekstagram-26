8e365db819850e240ff2a13fc458c1b7e9280df8
Leonid Chernyakov <scrltrb@gmail.com>
2022-06-07 15:38:12 +0300

    jsDoc + small changes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
 // Алгоритм взят с https://learn.javascript.ru/task/random-int-min-max
 // Если будет передан только один аргумент, то случайное число будет сгенерировано в промежутке от 0 до этого числа.
+/**
+ * Возвращает случайное число из диапазона.
+ * @param {number} min - минимальное число из диапазона
+ * @param {number} max - максимальное число из диапазона
+ * @returns {integer} - случайное число
+ */
 const getRandomNumber = (min, max = 0) => {
-  if (min <= max && min >= 0) {
+  if (max >= 0 && min >= 0) {
+    if (max < min) {
+      [min, max] = [max, min];
+    }
     return Math.abs(Math.round(min - 0.5 + Math.random() * (max - min + 1)));
-  } else if (max < min && max >= 0) {
-    // В формуле max и min поменяны местами
-    // Можно было реализовать с помощью обмена значений min и max, но посчитал, что такой вариант будет производительнее
-    return Math.abs(Math.round(max - 0.5 + Math.random() * (min - max + 1)));
   }
   throw new Error('Задан некорректный диапазон');
 };
 
-getRandomNumber(50);
+getRandomNumber(0, 10);
 
-const checkStringLength = (string, maxLength) => string.length <= maxLength;
+/**
+ * Проверяет допустимой ли длины строка
+ * @param {string} string - проверяемая строка
+ * @param {integer} maxLength - максимально допустимая длина для строки
+ * @returns {boolean} - истинно ли то, что строка допустимого размера
+ */
+const checkStringLength = (string, maxLength = 140) => string.length <= maxLength;
 
 checkStringLength('string');
