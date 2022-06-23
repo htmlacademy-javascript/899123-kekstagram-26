a69b63116580d7851653adcf2ebf4c680f23ab38
Keks <keks@htmlacademy.ru>
2022-06-23 14:52:28 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/render-thumbnails.js
@@ -0,0 +1,23 @@
+import { generatePublications } from './mocks/generate-mocks.js';
+
+import { openModal } from './big-picture-mode.js';
+
+const publications = generatePublications();
+
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesContainer = document.querySelector('.pictures');
+
+const thumbnailsFragment = document.createDocumentFragment();
+publications.forEach((publication) => {
+  const newThumbnail = thumbnailTemplate.cloneNode(true);
+
+  newThumbnail.querySelector('.picture__img').src = publication.url;
+  newThumbnail.querySelector('.picture__likes').textContent = publication.likes;
+  newThumbnail.querySelector('.picture__comments').textContent = publication.comments.length;
+
+  newThumbnail.addEventListener('click', () => openModal(publication));
+
+  thumbnailsFragment.appendChild(newThumbnail);
+});
+
+picturesContainer.appendChild(thumbnailsFragment);
